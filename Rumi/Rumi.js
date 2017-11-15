56bb037150a8cd8a084b69e3a973105a4922fd03
Hugo Kuipers <Hugokuipers@hotmail.com>
2017-11-15 16:22:39 +0100

    Work was not done

--- Rumi/Rumi.js
+++ Rumi/Rumi.js
@@ -1,65 +1,84 @@
 "use strict";
 var amountOfPlayers = 4;
 var players = [];
-var decksAmount = 2;
 var takenSeats = [];
-var boardStones = [];
-var allStones = {};
-for(var i = 1; i < 14; i++) {
-  for(var j = 0; j < 4; j++) {
-    var stoneColor = "Zwart";
-    switch(j%4) {
-      case 0:
-        stoneColor = "Blauw";
-        break;
-      case 1:
-        stoneColor = "Rood";
-        break;
-      case 2:
-        stoneColor = "Oranje";
-        break;
-    }
-    allStones[stoneColor+" "+i] = {
-      color: stoneColor,
-      value: i,
-      amount: 2
-    }
-  }
-}
-allStones["Joker"] = {
-  color: "Joker",
-  value: 0,
-  amount: 2
-}
+var decksAmount = 2;
+var pileStones;
+var boardStones;
+var allStones;
 
-var pileStones = [];
-for(var i = 1; i < 14; i++) {
-  for(var j = 0; j < 4; j++) {
-    for(var k = 0; k < decksAmount; k++) {
-    var stoneColor = "Zwart";
-      switch(j%4) {
-        case 0:
+var setDeck = function(deckAmount) {
+  if(!deckAmount) deckAmount = decksAmount;
+  var pileStones = [];
+  var boardStones = [];
+  // var allStones = {};
+  //
+  // for(var i = 1; i < 14; i++) {
+  //   for(var j = 0; j < 4; j++) {
+  //     var stoneColor = "Zwart";
+  //     switch(j%4) {
+  //       case 0:
+  //         stoneColor = "Blauw";
+  //         break;
+  //       case 1:
+  //         stoneColor = "Rood";
+  //         break;
+  //       case 2:
+  //         stoneColor = "Oranje";
+  //         break;
+  //     }
+  //     allStones[stoneColor+" "+i] = {
+  //       color: stoneColor,
+  //       value: i,
+  //       amount: deckAmount
+  //     }
+  //   }
+  // }
+  // allStones["Joker"] = {
+  //   color: "Joker",
+  //   value: 0,
+  //   amount: deckAmount
+  // }
+
+  for(var i = 1; i < 14; i++) {
+    for(var j = 0; j < 4; j++) {
+      for(var k = 0; k < deckAmount; k++) {
+        var stoneColor = "Zwart";
+        switch(j%4) {
+          case 0:
           stoneColor = "Blauw";
           break;
-        case 1:
+          case 1:
           stoneColor = "Rood";
           break;
-        case 2:
+          case 2:
           stoneColor = "Oranje";
           break;
+        }
+        pileStones.push({
+          name: stoneColor+" "+i,
+          color: stoneColor,
+          value: i
+        });
       }
     }
-    pileStones[stoneColor+" "+i] = {
-      name: stoneColor+" "+i,
-      color: stoneColor,
-      value: i
-    }
+  }
+  for(var k = 0; k < deckAmount; k++) {
+    pileStones.push({
+      name: "Joker",
+      color: "Joker",
+      value: 0
+    });
   }
 }
-pileStones["Joker"] = {
-  name: "Joker",
-  color: "Joker",
-  value: 0
+
+const createPlayTable = function() {
+  for(var i = 0; i < 13; i++) {
+    var createTr = taby.insertRow(i);
+    for(var j = 0; j < 13; j++) {
+      createTr.insertCell(j);
+    }
+  }
 }
 
 var Player = function(human, name, order, strength) {
@@ -67,7 +86,8 @@ var Player = function(human, name, order, strength) {
   if(!human) {
     this.name = markovNameGen("dutch");
     if(!strength) {
-      this.strength = Math.ceil(Math.random()*3);
+      // this.strength = Math.ceil(Math.random()*3);
+      this.strength = 1;
     }
     else {
       this.strength = strength;
@@ -95,9 +115,23 @@ var Player = function(human, name, order, strength) {
     }
   }
   this.hand = [];
+  this.pass = function() {
+
+  }
+  this.play = function() {
+
+  }
 }
 
-console.log(pileStones);
+setDeck();
 
 var guy = new Player();
-console.log(guy);
+
+var widthS = $(this.screen.width);
+var heigthS = $(this.screen.heigth);
+if($(this.screen.width)[0] < $(this.screen.height)[0]) {
+  $('#entire-playfield').css({
+    width: '100vw',
+    height: '100vw'
+  });
+}
